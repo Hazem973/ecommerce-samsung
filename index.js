5f80a92fa21fdc566936faf8a740a71c22bb0047
hash66 <morbak_hzm@hotmail.fr>
2023-03-11 16:56:46 +0100

    qts

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('aaa');
+
+
+// select all buttons with class "plus"
+var buttonsPlus = document.querySelectorAll('.plus')
+// select all buttons with class "plus"
+var buttonsMinus = document.querySelectorAll('.minus')
+// select all p with class "qty"
+var qtys = document.querySelectorAll('.qty')
+
+// buttonsPlus = [button1,button2]
+// qtys = [qty1,qty2]
+
+for (let index = 0; index < buttonsPlus.length; index++) {
+    
+    buttonsPlus[index].addEventListener('click', function()  {
+        qtys[index].innerHTML++
+     })
+     
+     buttonsMinus[index].addEventListener('click', function()  {
+        if(qtys[index].innerHTML > 0){
+             qtys[index].innerHTML--
+         }
+         
+      })  
+}
+
+
+
+
+
+
+// document.getElementById('ahla').innerHTML = "Ahla"
+
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", function () {
+//       return qte[i].innerHTML++;
+//     });
+//   }
+
